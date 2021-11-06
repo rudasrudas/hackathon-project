3b23901afcf99c581b720e2c696145d7117ad972
justaszdanavicius <zdanaviciusjustas1@gmail.com>
2021-11-06 17:32:05 +0100

    Updated

---
+++ Hackathon-project/Hackathon-project/Main.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Reflection;
+
+namespace Hackathon_project
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Title", "Hello World!");
+
+            return Result.Succeeded;
+        }
+    }
+}
+}
